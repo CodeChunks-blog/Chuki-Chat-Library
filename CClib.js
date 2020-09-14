b0cb06ac0aa01d80b89a9a35a046834c8ef4e3a5
iamashwin99 <46030335+iamashwin99@users.noreply.github.com>
2020-09-14 12:37:20 +0530

    Create CClib.js

---
+++ CClib.js
@@ -0,0 +1,86 @@
+var speaker = new p5.Speech();
+
+function listent() {
+  let lang = navigator.language || 'en-US';
+  var speech = new p5.SpeechRec(lang); // speech recognition object (will prompt for mic access)
+  speech.onResult = showResult; // bind callback function to trigger when speech is recognized
+  speech.start(); // start listening
+  function showResult() {
+
+    if (speech.resultValue) {
+      console.log(speech.resultString); // log the result
+      let msg = speech.resultString;
+      sendUserMessage(msg);
+      respond(msg);
+    }
+
+  }
+
+
+  //console.log("clicked");
+
+
+
+}
+
+
+
+
+function getTime() {
+  let h = hour();
+  let m = minute();
+  let ampm = h <= 12 ? "AM" : "PM"
+  return "" + h % 12 + ":" + m + " " + ampm
+}
+
+function escape(txt) {
+  let newtxt = "";
+  for (let char in txt) {
+    char = txt.charAt(char);
+    if (char == "<") {
+      newtxt += "&lt"
+    } else if (char == ">") {
+      newtxt += "&gt"
+    } else newtxt += char
+  }
+  return newtxt
+}
+
+function sendMessage(msg) {
+  if (msg != "") {
+
+    setTimeout(() => {
+      let reply = createDiv('<img src="chukki.png" alt="Avatar" style="width:100%;"><p>' + msg + '</p><span class="time-right">' + getTime() + '</span>').addClass("container");
+      reply.parent("txtScreen");
+
+      select("#msg").value("");
+      let objDiv = document.getElementById("txtScreen");
+      objDiv.scrollTop = objDiv.scrollHeight;
+    }, 1000);
+  }
+  
+  
+  speaker.speak(msg);
+}
+
+function sendUserMessage(msg) {
+  msg = escape(msg);
+  let div = createDiv('<img src="user.webp" alt="Avatar" class="right" style="width:100%;"><p class="right-txt">' + msg + '</p><span class="time-left">' + getTime() + '</span>').addClass("container").addClass("user");
+  div.parent("txtScreen");
+  let objDiv = document.getElementById("txtScreen");
+  objDiv.scrollTop = objDiv.scrollHeight;
+}
+
+function send() {
+  let msg = select("#msg").value();
+  if (msg != "") {
+    sendUserMessage(msg);
+    respond(msg);
+  }
+}
+
+function keyPressed() {
+  if (keyCode == 13) {
+    send()
+  }
+}
