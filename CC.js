4126561a80fc9127781eb96a38d30fea9d713f44
iamaashwin99 <iamaashwin99@gmail.com>
2020-09-14 13:08:03 +0530

    webp images

---
+++ CC.js
@@ -0,0 +1,7 @@
+/*Your instructions to Chukki goes here*/
+sendMessage("Hi, my name is Chukki what is your name?");
+
+function respond(input) {
+  sendMessage("Hi "+ input + ", nice to meet you!");
+  //sendMessage(input);
+}
